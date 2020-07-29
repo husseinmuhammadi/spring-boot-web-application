82f542e91a2588aee8e827d831cbb3909299a83f
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-07-29 05:29:39 -0700

    Spring Boot Console Application

---
+++ src/main/java/com/javastudio/tutorial/SpringBootConsoleApplication.java
@@ -0,0 +1,50 @@
+package com.javastudio.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootConsoleApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("STARTING THE APPLICATION");
+        SpringApplication.run(SpringBootConsoleApplication.class, args);
+        LOGGER.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) {
+        LOGGER.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOGGER.info("args[{}]: {}", i, args[i]);
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            LOGGER.info("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(LOGGER::info);
+        };
+    }
+}
